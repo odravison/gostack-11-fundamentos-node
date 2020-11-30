7a167c5634313a5f05d2161cc8893b8465654b2f
Odravison Amaral <odravison.jr.ext@conductor.com.br>
2020-11-30 01:38:18 -0300

    Complete the challenge

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,10 @@
 import Transaction from '../models/Transaction';
 
+interface TransactionDTO {
+  value: number;
+  type: 'outcome' | 'income';
+  title: string;
+}
 interface Balance {
   income: number;
   outcome: number;
@@ -14,15 +19,39 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const incomeAmount = this.transactions.reduce((accumulator, current) => {
+      if (current.type === 'income') {
+        accumulator.value += current.value;
+        return accumulator;
+      }
+      return accumulator;
+    }, new Transaction({ title: '', value: 0, type: 'income' }));
+
+    const outcomeAmount = this.transactions.reduce((accumulator, current) => {
+      if (current.type === 'outcome') {
+        accumulator.value += current.value;
+        return accumulator;
+      }
+      return accumulator;
+    }, new Transaction({ title: '', value: 0, type: 'income' }));
+
+    return {
+      income: incomeAmount.value,
+      outcome: outcomeAmount.value,
+      total: incomeAmount.value - outcomeAmount.value,
+    };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: TransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,11 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +13,22 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    if (!title || !value || !type) {
+      throw new Error('Required value missed. Please, provide it!');
+    }
+
+    const balance = this.transactionsRepository.getBalance();
+    if (type === 'outcome' && balance.total < value)
+      throw Error('Valor saída maior que em caixa');
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
